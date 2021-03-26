a2ea09ce25314fd9d21de30a4a63ed79ed7017b7
Walter Ishibashi <wishibashi@gmail.com>
2021-03-26 19:54:08 +0000

    Refactoring the tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -6,20 +6,24 @@ RSpec.describe 'Users API', type: :request do
                                         # que o build só cria a instância em memória
     let(:user_id) { user.id }  
     
+    let(:headers) do
+        {
+            'Accept' => 'application/vnd.taskmanager.v1',
+            'Content-Type' => Mime[:json].to_s              # as info enviadas nas operações devem todos ter formato json
+        }
+    end
+
     before { host! 'api.qualquerdominio.test' }
 
     describe 'GET /users/:id' do
         before do
-            headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
-            get "/users/#{user_id}", params: {}, headers: headers    # define a URL, o corpo em branco, headers do get
+             get "/users/#{user_id}", params: {}.to_json, headers: headers    # define a URL, o corpo em branco, headers do get
                                                                     # para se concatenar variável não se pode utilizar aspas simples
         end
 
         context 'when the user exists' do
             it 'returns the user' do
-                user_response = JSON.parse(response.body, symbolize_names: true) # symbolyze_names=true faz com que
-                                                                # JSON.parse use index tipo simbolo ao invés de string
-                expect(user_response[:id]).to eq(user_id)  # texta se o id retornado é igual ao user_id
+                expect(json_body[:id]).to eq(user_id)  # texta se o id retornado é igual ao user_id
             end
 
             it 'returns status code 200' do  # 200=OK https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
@@ -37,9 +41,8 @@ RSpec.describe 'Users API', type: :request do
 
     describe 'POST /users' do
         before do
-            headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
             # user_params = { email: 'teste@email.com', password: '123456', password_confirmation: '123456' }
-            post '/users', params: { user: user_params }, headers: headers
+            post '/users', params: { user: user_params }.to_json, headers: headers
         end
         
         context 'when the request param are valid' do
@@ -50,9 +53,7 @@ RSpec.describe 'Users API', type: :request do
             end
 
             it 'returns json data for the created user' do
-                user_response = JSON.parse(response.body, symbolize_names: true) # symbolyze_names=true faz com que
-                                                                    # JSON.parse use index tipo simbolo ao invés de string
-                expect(user_response[:email]).to eq(user_params[:email])   
+                expect(json_body[:email]).to eq(user_params[:email])   
             end
         end
    
@@ -64,8 +65,7 @@ RSpec.describe 'Users API', type: :request do
             end
         
             it 'returns json data for the error' do
-                user_response = JSON.parse(response.body, symbolize_names: true)
-                expect(user_response).to have_key(:errors)
+                expect(json_body).to have_key(:errors)
             end
         end
    
@@ -73,8 +73,7 @@ RSpec.describe 'Users API', type: :request do
 
     describe 'PUT /users/:id' do
         before do
-            headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
-            put "/users/#{user_id}", params: { user: user_params }, headers: headers
+             put "/users/#{user_id}", params: { user: user_params }.to_json, headers: headers
         end
         
         context 'when the request param are valid' do
@@ -85,9 +84,7 @@ RSpec.describe 'Users API', type: :request do
             end
 
             it 'returns json data for the updated user' do
-                user_response = JSON.parse(response.body, symbolize_names: true) # symbolyze_names=true faz com que
-                                                                    # JSON.parse use index tipo simbolo ao invés de string
-                expect(user_response[:email]).to eq(user_params[:email])   
+                expect(json_body[:email]).to eq(user_params[:email])   
             end
         end
    
@@ -99,8 +96,7 @@ RSpec.describe 'Users API', type: :request do
             end
 
             it 'returns json data for the error' do
-                user_response = JSON.parse(response.body, symbolize_names: true)
-                expect(user_response).to have_key(:errors)
+                expect(json_body).to have_key(:errors)
             end
         end
    
@@ -108,8 +104,7 @@ RSpec.describe 'Users API', type: :request do
 
     describe 'DELETE /users/:id' do
         before do
-            headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
-            delete "/users/#{user_id}", params: {}, headers: headers    # define a URL, o corpo em branco, headers do get
+             delete "/users/#{user_id}", params: {}.to_json, headers: headers    # define a URL, o corpo em branco, headers do get
                                                                        # para se concatenar variável não se pode utilizar aspas simples
         end
        
